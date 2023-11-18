f616e623ccc26ce48f343e283ea24137b2af1f0f
lixin <lixin@bttc.edu.cn>
2023-11-18 20:52:52 +0800

    code

---
+++ briup/src/com/atguigu/day1/HelloTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.day1;
+
+/**
+ * ClassName:HelloTest
+ * Description:
+ *
+ * @Authonr 李欣
+ * @Create 2023/11/18/018 20:50
+ * @Version 1.0
+ */
+public class HelloTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
