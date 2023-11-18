4baee51578a41abcca78873519c98a3e9e6e7f21
lixin <lixin@bttc.edu.cn>
2023-11-18 21:20:11 +0800

    code

---
+++ briup/src/com/atguigu/day1/Test.java
@@ -0,0 +1,15 @@
+package com.atguigu.day1;
+
+/**
+ * ClassName:Test
+ * Description:
+ *
+ * @Authonr 李欣
+ * @Create 2023/11/18/018 21:19
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
